bed05e775c2934f649ce849ded68da41ca6b6006
Marco77577 <marco.studer@srf.ch>
2026-02-24 13:44:44 +0100

    Introduce color honey

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className={`py-12`}>
             <div className={`mx-auto container flex items-center gap-4`}>
-                <Honeycomb strokeWidth={8} className={`h-8 text-yellow-500`}/>
+                <Honeycomb strokeWidth={8} className={`h-8 text-honey`}/>
                 <h1 className={`text-3xl font-semibold leading-10 tracking-tight`}>honeybee</h1>
             </div>
         </header>
