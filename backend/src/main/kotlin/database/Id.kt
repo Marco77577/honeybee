115bb5d27e42e612cd1453a0d9b2fb9c4b79ca85
Marco77577 <marco.studer@srf.ch>
2026-02-26 10:02:34 +0100

    Refactor Id object to generate id directly using table, not table name

--- backend/src/main/kotlin/database/Id.kt
+++ backend/src/main/kotlin/database/Id.kt
@@ -1,23 +1,24 @@
 package database
 
 import database.currency.Currencies
+import org.jetbrains.exposed.sql.Table
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
 @OptIn(ExperimentalUuidApi::class)
 object Id {
 
-    fun currency() = generate(Currencies.tableName)
+    fun currency() = generate(Currencies)
 
     /**
      * Generates a prefixed UUID.
-     * @param tableName The table name from which the prefix is derived.
+     * @param table The table from which the prefix is derived.
      * @return The generated UUID.
      */
-    private fun generate(tableName: String): String {
-        val prefix = tableName.substring(
+    private fun generate(table: Table): String {
+        val prefix = table.tableName.substring(
             startIndex = 0,
-            endIndex = 3.coerceAtMost(tableName.length)
+            endIndex = 3.coerceAtMost(table.tableName.length)
         )
         return "${prefix}_${Uuid.Companion.generateV7()}"
     }
