80ce0785185e88bfaf1ff3dd1be28091a62a74fa
Marco77577 <marco.studer@srf.ch>
2026-02-26 10:01:37 +0100

    Make id of currencies table a unique index

--- backend/src/main/kotlin/database/currency/Currencies.kt
+++ backend/src/main/kotlin/database/currency/Currencies.kt
@@ -5,7 +5,7 @@ import org.jetbrains.exposed.sql.javatime.CurrentDateTime
 import org.jetbrains.exposed.sql.javatime.datetime
 
 object Currencies : Table("currency") {
-    val id = varchar("id", 255)
+    val id = varchar("id", 255).uniqueIndex()
     val name = varchar("name", 255)
     val abbreviation = varchar("abbreviation", 3).uniqueIndex()
     val manualExchangeRate = double("manual_exchange_rate").nullable()
