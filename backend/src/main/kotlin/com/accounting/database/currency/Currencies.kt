a279caeed0e10634b86b55f6b95040fd92af8eee
Marco77577 <marco.studer@srf.ch>
2026-02-26 12:10:14 +0100

    Change double to decimal for more precision

--- backend/src/main/kotlin/com/accounting/database/currency/Currencies.kt
+++ backend/src/main/kotlin/com/accounting/database/currency/Currencies.kt
@@ -8,7 +8,7 @@ object Currencies : Table("currency") {
     val id = varchar("id", 255).uniqueIndex()
     val name = varchar("name", 255)
     val abbreviation = varchar("abbreviation", 3)
-    val manualExchangeRate = double("manual_exchange_rate").nullable()
+    val manualExchangeRate = decimal("manual_exchange_rate", 19, 4).nullable()
     val updatedAt = datetime("updated_at").defaultExpression(CurrentDateTime)
     val createdAt = datetime("created_at").defaultExpression(CurrentDateTime)
     override val primaryKey = PrimaryKey(id)
