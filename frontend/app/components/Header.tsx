3a5d23bd590ce8f5b245c89e33ff223652787cd4
Marco77577 <marco.studer@srf.ch>
2026-03-04 08:48:20 +0100

    Style login and sign up buttons

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -2,10 +2,16 @@ import Honeycomb from "@/app/components/Honeycomb";
 
 export default function Header() {
     return (
-        <header className={`py-12`}>
-            <div className={`mx-auto container flex items-center gap-4`}>
-                <Honeycomb strokeWidth={8} className={`h-8 text-honey`}/>
-                <h1 className={`text-3xl`}>honeybee</h1>
+        <header>
+            <div className={`mx-auto container flex items-center justify-between py-4`}>
+                <div className={`flex items-center gap-3`}>
+                    <Honeycomb strokeWidth={8} className={`h-8 text-honey`}/>
+                    <h1 className={`text-3xl`}>honeybee</h1>
+                </div>
+                <div className={`flex items-center gap-3`}>
+                    <button className={`rounded-md px-3 py-2 border border-button-secondary-border bg-button-secondary-background hover:bg-button-secondary-background-hover text-button-secondary-foreground text-sm`}>Log In</button>
+                    <button className={`rounded-md px-3 py-2 border border-button-border bg-button-background hover:bg-button-background-hover text-button-foreground text-sm`}>Sign Up</button>
+                </div>
             </div>
         </header>
     )
