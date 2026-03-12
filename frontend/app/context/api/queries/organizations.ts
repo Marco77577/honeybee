c9b84d2249c8f0d7cfad7d38014e01f959e5cdad
Marco77577 <marco.studer@srf.ch>
2026-03-12 15:31:59 +0100

    Reorganize organization queries into queries and mutations

--- frontend/app/context/api/queries/organizations.ts
+++ frontend/app/context/api/queries/organizations.ts
@@ -1,15 +1,40 @@
-import {OrganizationApi} from "@/app/generated/api";
+import {ComAccountingApiOrganizationModelNewOrganization, OrganizationApi} from "@/app/generated/api";
 import {useOrganizationApi} from "@/app/context/api/ApiProvider";
-import {useQuery} from "@tanstack/react-query";
+import {mutationOptions, queryOptions, useMutation, useQuery, useQueryClient} from "@tanstack/react-query";
 
-export const organizationQueries = {
-    all: (api: OrganizationApi) => ({
-        queryKey: ["organizations"],
-        queryFn: ({signal}: { signal: AbortSignal }) => api.apiV1OrganizationGet({signal}),
-    })
+export const organizationKeys = {
+    all: ["organizations"] as const,
+};
+
+export const organization = {
+    queries: {
+        all: (api: OrganizationApi) => queryOptions({
+            queryKey: organizationKeys.all,
+            queryFn: ({signal}: { signal: AbortSignal }) => api.apiV1OrganizationGet({signal}),
+        }),
+    },
+    mutations: {
+        create: (api: OrganizationApi) => mutationOptions({
+            mutationKey: ["create-organization"],
+            mutationFn: (organization: ComAccountingApiOrganizationModelNewOrganization) => api.apiV1OrganizationPost(
+                {comAccountingApiOrganizationModelNewOrganization: organization}
+            ),
+        }),
+    },
 };
 
 export function useOrganizationsQuery() {
     const api = useOrganizationApi();
-    return useQuery(organizationQueries.all(api));
+    return useQuery(organization.queries.all(api));
+}
+
+export function useCreateOrganizationMutation() {
+    const queryClient = useQueryClient();
+    const api = useOrganizationApi();
+    return useMutation({
+        ...organization.mutations.create(api),
+        onSuccess: () => queryClient.invalidateQueries({
+            queryKey: organizationKeys.all,
+        }),
+    })
 }
