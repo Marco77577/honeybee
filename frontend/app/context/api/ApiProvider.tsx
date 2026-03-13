87c140171826afa88a21a0691c40e002e6685fab
Marco77577 <marco.studer@srf.ch>
2026-03-13 11:28:25 +0100

    Add the CurrencyApi to the ApiProvider

--- frontend/app/context/api/ApiProvider.tsx
+++ frontend/app/context/api/ApiProvider.tsx
@@ -1,19 +1,26 @@
 "use client"
 
 import React, {createContext, useContext, useMemo} from "react";
-import {Configuration, OrganizationApi} from "@/app/generated/api";
+import {Configuration, CurrencyApi, OrganizationApi} from "@/app/generated/api";
 import {useAuth} from "react-oidc-context";
 
 interface ApiProvider {
-    organizationApi: OrganizationApi
+    organizationApi: OrganizationApi,
+    currencyApi: CurrencyApi,
 }
 
 export const ApiContext = createContext<ApiProvider | null>(null)
 
 export function useOrganizationApi() {
-    const api = useContext(ApiContext);
-    if (!api) throw new Error("api provider not initialized");
-    return api.organizationApi
+    const context = useContext(ApiContext);
+    if (!context) throw new Error("api provider not initialized");
+    return context.organizationApi
+}
+
+export function useCurrencyApi() {
+    const context = useContext(ApiContext);
+    if (!context) throw new Error("api provider not initialized");
+    return context.currencyApi
 }
 
 export function ApiProvider({children}: { children: React.ReactNode }) {
@@ -25,7 +32,8 @@ export function ApiProvider({children}: { children: React.ReactNode }) {
                 accessToken: `Bearer ${auth.user?.access_token}`,
             });
             return {
-                organizationApi: new OrganizationApi(config)
+                organizationApi: new OrganizationApi(config),
+                currencyApi: new CurrencyApi(config),
             };
         },
         [auth.user?.access_token]
