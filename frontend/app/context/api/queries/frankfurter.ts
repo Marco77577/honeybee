90bfaebe7aaa28673b66d44b2d044a3e1921f905
Marco77577 <marco.studer@srf.ch>
2026-03-13 14:10:37 +0100

    Implement Frankfurter API to get exchange rates

---
+++ frontend/app/context/api/queries/frankfurter.ts
@@ -0,0 +1,45 @@
+"use client"
+
+import {queryOptions, useQuery} from "@tanstack/react-query";
+
+export class FrankfurterExchangeRates {
+
+    constructor(
+        public readonly base: string,
+        public readonly date: string,
+        public readonly rates: Map<string, number>,
+    ) {
+    }
+}
+
+export const frankfurterKeys = {
+    latest: ["frankfurter", "latest"] as const,
+};
+
+export const frankfurter = {
+    queries: {
+        latest: (baseCurrency: string | undefined) => queryOptions({
+            queryKey: [...frankfurterKeys.latest, baseCurrency],
+            queryFn: async ({signal}: {
+                signal: AbortSignal
+            }) => {
+                const response = await fetch(`https://api.frankfurter.dev/v1/latest?base=${baseCurrency}`, {
+                    headers: {'Content-Type': 'application/json'},
+                    signal: signal,
+                });
+                const json = await response.json();
+
+                return new FrankfurterExchangeRates(
+                    json.base,
+                    json.date,
+                    new Map(Object.entries(json.rates))
+                );
+            },
+            enabled: baseCurrency !== undefined,
+        }),
+    },
+};
+
+export function useFrankfurterLatest(baseCurrency: string | undefined) {
+    return useQuery(frankfurter.queries.latest(baseCurrency));
+}
