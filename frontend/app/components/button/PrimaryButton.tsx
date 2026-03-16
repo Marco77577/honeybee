fbc25490162ad1b387df79582ddabc83cc918dae
Marco77577 <marco.studer@srf.ch>
2026-03-16 10:22:50 +0100

    Allow to disable a PrimaryButton component

--- frontend/app/components/button/PrimaryButton.tsx
+++ frontend/app/components/button/PrimaryButton.tsx
@@ -3,18 +3,29 @@ import clsx from "clsx";
 
 interface PrimaryButtonProps {
     title: string,
+    disabled: boolean,
 }
 
 export default function PrimaryButton({
                                           title,
+                                          disabled,
                                           className,
                                           ...props
                                       }: PrimaryButtonProps & React.HTMLAttributes<HTMLButtonElement>) {
     return (
         <button
             {...props}
-            className={clsx(`rounded-md px-3 py-2 border border-button-border bg-button-background hover:bg-button-background-hover text-button-foreground text-sm`, className)}>
+            disabled={disabled}
+            className={clsx(
+                `rounded-md px-3 py-2 border border-button-border bg-button-background hover:bg-button-background-hover text-button-foreground text-sm`,
+                className,
+                disabled && `bg-button-background-hover/50!`
+            )}>
             <span>{title}</span>
         </button>
     )
+}
+
+PrimaryButton.defaultProps = {
+    disabled: false
 }
