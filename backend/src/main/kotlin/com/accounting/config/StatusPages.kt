29212f34600826c586374bb676f6fa6d128ddc59
Marco77577 <marco.studer@srf.ch>
2026-03-17 10:50:19 +0100

    Implement exception handler

--- backend/src/main/kotlin/com/accounting/config/StatusPages.kt
+++ backend/src/main/kotlin/com/accounting/config/StatusPages.kt
@@ -2,28 +2,64 @@ package com.accounting.config
 
 import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.plugins.requestvalidation.RequestValidationException
+import io.ktor.server.plugins.requestvalidation.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
 
 class NotAuthenticatedException : Exception("user is not authenticated")
 class NotAllowedToViewOrganization : Exception("user may not view organization")
 class NotAllowedToWriteOrganization : Exception("user may not write organization")
 
+@Serializable
+data class Error(
+    val statusCode: Int,
+    val errorCode: Int,
+    val message: String,
+)
+
 fun Application.configureStatusPages() {
     install(StatusPages) {
         exception<RequestValidationException> { call, cause ->
-            call.respond(HttpStatusCode.BadRequest, cause.message ?: "Bad Request")
+            call.respond(
+                HttpStatusCode.BadRequest,
+                Error(
+                    statusCode = HttpStatusCode.BadRequest.value,
+                    errorCode = 1,
+                    message = cause.message ?: "Bad Request"
+                )
+            )
         }
         exception<NotAllowedToWriteOrganization> { call, cause ->
-            call.respond(HttpStatusCode.Forbidden, cause.message ?: "Forbidden")
+            call.respond(
+                HttpStatusCode.Forbidden,
+                Error(
+                    statusCode = HttpStatusCode.Forbidden.value,
+                    errorCode = 2,
+                    message = cause.message ?: "Forbidden"
+                )
+            )
         }
         exception<NotAllowedToViewOrganization> { call, cause ->
-            call.respond(HttpStatusCode.Forbidden, cause.message ?: "Forbidden")
+            call.respond(
+                HttpStatusCode.Forbidden,
+                Error(
+                    statusCode = HttpStatusCode.Forbidden.value,
+                    errorCode = 3,
+                    message = cause.message ?: "Forbidden"
+                )
+            )
         }
         exception<NotAuthenticatedException> { call, cause ->
-            call.respond(HttpStatusCode.Unauthorized, cause.message ?: "Unauthorized")
+            call.respond(
+                HttpStatusCode.Unauthorized,
+                Error(
+                    statusCode = HttpStatusCode.Unauthorized.value,
+                    errorCode = 4,
+                    message = cause.message ?: "Unauthorized"
+                )
+            )
         }
         exception<Throwable> { call, cause ->
             call.respondText(text = "500: $cause", status = HttpStatusCode.InternalServerError)
