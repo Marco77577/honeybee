a80ec3510b7c3d803f075e3f695f1355bb43926c
Marco77577 <marco.studer@srf.ch>
2026-03-19 16:22:24 +0100

    Switch to dialogs in the currency page

--- frontend/app/currency/page.tsx
+++ frontend/app/currency/page.tsx
@@ -1,25 +1,18 @@
 "use client"
 
 import Heading1 from "@/app/components/title/Heading1";
-import {Banknote, BanknoteX, BookSearch, Pencil, Trash} from "lucide-react";
-import {
-    Currency as PublicCurrency,
-    useCurrenciesQuery,
-    useDeleteCurrencyMutation,
-    useMainCurrency
-} from "@/app/context/api/queries/currencies";
+import {Banknote, BookSearch, Pencil, Trash} from "lucide-react";
+import {useCurrenciesQuery, useMainCurrency} from "@/app/context/api/queries/currencies";
 import {Skeleton} from "@radix-ui/themes";
 import CurrencyAbbreviation from "@/app/currency/CurrencyAbbreviation";
 import PrimaryButton from "@/app/components/button/PrimaryButton";
-import React, {useState} from "react";
-import {AutoHeight} from "@/app/components/AutoHeight";
+import React from "react";
 import clsx from "clsx";
 import {useFrankfurterLatest} from "@/app/context/api/queries/frankfurter";
 import UpsertCurrency from "@/app/currency/UpsertCurrency";
-import {useOrganization} from "@/app/context/OrganizationProvider";
-import Heading2 from "@/app/components/title/Heading2";
-import SecondaryButton from "@/app/components/button/SecondaryButton";
 import ClickableIcon from "@/app/components/ClickableIcon";
+import {Dialog, DialogContext} from "@/app/components/Dialog";
+import DeleteCurrency from "@/app/currency/DeleteCurrency";
 
 export function calculateLeftHandExchangeRate(rate: number) {
     let left = 1;
@@ -62,67 +55,13 @@ function LoadingCurrency() {
 
 export default function Currency() {
     // LOAD DATA FROM BACKEND
-    const organization = useOrganization();
     const mainCurrency = useMainCurrency();
     const {data: currencies} = useCurrenciesQuery();
     const {data: latestExchangeRates} = useFrankfurterLatest(mainCurrency?.abbreviation);
 
-    // UPSERT CURRENCY STATES & HANDLERS
-    const [isCreatingCurrency, setIsCreatingCurrency] = useState<boolean>(false);
-    const [isUpdatingCurrency, setIsUpdatingCurrency] = useState<boolean>(false);
-    const [isDeletingCurrency, setIsDeletingCurrency] = useState<boolean>(false);
-    const [isServerError, setIsServerError] = useState<boolean>(false);
-    const [updatingCurrency, setUpdatingCurrency] = useState<PublicCurrency | undefined>(undefined);
-    const [deletingCurrency, setDeletingCurrency] = useState<PublicCurrency | undefined>(undefined);
-
-    const handleEditClick = (currency: PublicCurrency) => {
-        setUpdatingCurrency(currency);
-        setIsUpdatingCurrency(true);
-    }
-
-    const handleDeleteClick = (currency: PublicCurrency) => {
-        setDeletingCurrency(currency);
-        setIsDeletingCurrency(true);
-    }
-
-    const handleEditCurrencyDoneClick = () => {
-        setUpdatingCurrency(undefined);
-        setIsUpdatingCurrency(false);
-    }
-
-    const handleDeleteCurrencyDoneClick = () => {
-        setDeletingCurrency(undefined);
-        setIsDeletingCurrency(false);
-    }
-
-    const deleteCurrency = useDeleteCurrencyMutation();
-
-    const handleCurrencyDelete = () => {
-        const organizationId = organization?.id;
-        const currencyId = deletingCurrency?.id;
-        if (organizationId == null || !currencyId) {
-            setIsServerError(true);
-            return;
-        }
-
-        deleteCurrency.mutate(
-            {
-                organizationId,
-                currencyId
-            },
-            {
-                onSuccess: () => {
-                    handleDeleteCurrencyDoneClick();
-                    setIsServerError(false);
-                },
-                onError: () => setIsServerError(true)
-            }
-        );
-    }
-
     return (
         <div className={`flex flex-col gap-6 py-16`}>
-            <div className={`mx-auto container px-4 md:px-0`}>
+            <div className={`mx-auto container px-4 md:px-0 flex flex-col gap-6`}>
                 <div className={`w-full max-w-xl mx-auto flex flex-col gap-6`}>
                     <Heading1 title={`Currencies`}
                               icon={<Banknote size={40} strokeWidth={1}/>}/>
@@ -130,48 +69,6 @@ export default function Currency() {
                                                                       target={`_blank`}>Frankfurter
                         API</a>.</p>
                 </div>
-            </div>
-
-            {/* UPDATE CURRENCY */}
-            <AutoHeight open={isUpdatingCurrency}>
-                <div className={`bg-popover-element-hover w-full`}>
-                    <div className={`mx-auto container px-4 md:px-0`}>
-                        <UpsertCurrency key={updatingCurrency?.id}
-                                        edit={updatingCurrency}
-                                        onSuccess={handleEditCurrencyDoneClick}/>
-                    </div>
-                </div>
-            </AutoHeight>
-
-            {/* DELETE CURRENCY */}
-            <AutoHeight open={isDeletingCurrency}>
-                <div className={`bg-popover-element-hover w-full`}>
-                    <div className={`mx-auto container px-4 md:px-0`}>
-                        <div className={`w-full max-w-xl mx-auto flex flex-col gap-3 py-4`}>
-                            <div className={`flex flex-col gap-1`}>
-                                <Heading2 title={`Delete Currency`}
-                                          icon={<BanknoteX size={40} strokeWidth={1}/>}/>
-                                <p>Are you sure you want to delete currency <span
-                                    className={`inline-flex items-center gap-2`}>
-                                    <CurrencyAbbreviation
-                                        abbreviation={deletingCurrency?.abbreviation}/><span>{deletingCurrency?.name}</span></span>?
-                                </p>
-                                <p>This cannot be undone.</p>
-                                <div className={`flex items-center justify-end gap-3`}>
-                                    <SecondaryButton title={`Delete`}
-                                                     loading={deleteCurrency.isPending}
-                                                     onClick={handleCurrencyDelete}/>
-                                    <PrimaryButton title={`Cancel`}
-                                                   onClick={handleDeleteCurrencyDoneClick}/>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </AutoHeight>
-
-            {/* TABLE */}
-            <div className={`mx-auto container px-4 md:px-0`}>
                 <div className={`w-full max-w-xl mx-auto flex flex-col gap-3`}>
                     <div
                         className={`grid grid-cols-[min-content_1fr_min-content_min-content_min-content] md:grid-cols-[min-content_1fr_1fr_min-content_min-content_min-content] gap-x-3 rounded-lg p-2 bg-input-text-background border border-input-text-border text-input-text-foreground outline-3 outline-transparent hover:outline-input-text-border-outline`}>
@@ -220,35 +117,60 @@ export default function Currency() {
                                             <span
                                                 className={clsx(`text-xs rounded-md bg-popover-element-hover p-1`)}>Manual</span>
                                         </div>
-                                        <ClickableIcon title={`Edit Currency`}
-                                                       onClick={() => handleEditClick(currency)}>
-                                            <Pencil size={20} strokeWidth={1}/>
-                                        </ClickableIcon>
-                                        <ClickableIcon title={`Delete Currency`}
-                                                       onClick={() => handleDeleteClick(currency)}>
-                                            <Trash size={20} strokeWidth={1}/>
-                                        </ClickableIcon>
+                                        <Dialog>
+                                            <Dialog.Trigger tabIndex={0}>
+                                                <ClickableIcon title={`Edit Currency`}>
+                                                    <Pencil size={20} strokeWidth={1}/>
+                                                </ClickableIcon>
+                                            </Dialog.Trigger>
+                                            <Dialog.Content>
+                                                <DialogContext.Consumer>
+                                                    {({close}) => (
+                                                        <UpsertCurrency
+                                                            edit={currency}
+                                                            onSuccess={close}
+                                                        />
+                                                    )}
+                                                </DialogContext.Consumer>
+                                            </Dialog.Content>
+                                        </Dialog>
+                                        <Dialog>
+                                            <Dialog.Trigger tabIndex={0}>
+                                                <ClickableIcon title={`Delete Currency`}>
+                                                    <Trash size={20} strokeWidth={1}/>
+                                                </ClickableIcon>
+                                            </Dialog.Trigger>
+                                            <Dialog.Content>
+                                                <DialogContext.Consumer>
+                                                    {({close}) => (
+                                                        <DeleteCurrency
+                                                            currency={currency}
+                                                            onSuccess={close}
+                                                        />
+                                                    )}
+                                                </DialogContext.Consumer>
+                                            </Dialog.Content>
+                                        </Dialog>
                                     </div>
                                 )) ?? Array.from({length: 5}, (_, i) => <LoadingCurrency key={i}/>)
                         }
                     </div>
-                    <AutoHeight open={!isCreatingCurrency}>
-                        <div className={`flex justify-end`}>
-                            <PrimaryButton title={`Add Currency`}
-                                           onClick={() => setIsCreatingCurrency(true)}/>
-                        </div>
-                    </AutoHeight>
-                </div>
-            </div>
-
-            {/* CREATE CURRENCY */}
-            <AutoHeight open={isCreatingCurrency}>
-                <div className={`bg-popover-element-hover w-full`}>
-                    <div className={`mx-auto container px-4 md:px-0`}>
-                        <UpsertCurrency onSuccess={() => setIsCreatingCurrency(false)}/>
+                    <div className={`flex justify-end`}>
+                        <Dialog>
+                            <Dialog.Trigger tabIndex={0}>
+                                <PrimaryButton title={`Add Currency`}/>
+                            </Dialog.Trigger>
+                            <Dialog.Content>
+                                <DialogContext.Consumer>
+                                    {({close}) => (
+                                        <UpsertCurrency onSuccess={close}/>
+                                    )}
+                                </DialogContext.Consumer>
+                            </Dialog.Content>
+                        </Dialog>
                     </div>
                 </div>
-            </AutoHeight>
+            </div>
         </div>
     )
 }
