6507e241b078e1265d0c755309214282e995de02
Marco77577 <marco.studer@srf.ch>
2026-03-26 15:26:49 +0100

    Refactor the SelectField to allow to query options

--- frontend/app/components/form/SelectField.tsx
+++ frontend/app/components/form/SelectField.tsx
@@ -2,26 +2,90 @@ import React, {ReactElement, useId, useState} from "react";
 import clsx from "clsx";
 import {AutoHeight} from "@/app/components/AutoHeight";
 import PopoverDivider from "@/app/components/header/profile/PopoverDivider";
+import InputField from "@/app/components/form/InputField";
+import {Skeleton} from "@radix-ui/themes";
 
-interface SelectFieldProps {
+interface SelectFieldProps<T> {
     icon: ReactElement,
-    value: string | undefined,
-    options: string[],
+    value?: T | null,
+    options: T[],
     placeholder?: string,
-    onValueChange: (value: string) => void,
+    onValueChange: (value: T) => void
+    toText?: (value: T) => string,
 }
 
-export default function SelectField({
-                                        icon,
-                                        value,
-                                        options,
-                                        placeholder,
-                                        onValueChange,
-                                        className,
-                                        ...props
-                                    }: SelectFieldProps & React.HTMLAttributes<HTMLDivElement>) {
+export default function SelectField<T>({
+                                           icon,
+                                           value,
+                                           options,
+                                           placeholder,
+                                           onValueChange,
+                                           toText = (value: T) => value as string,
+                                           className,
+                                           ...props
+                                       }: SelectFieldProps<T> & React.HTMLAttributes<HTMLDivElement>) {
     const id = useId();
+    const [, setSelected] = useState<T | null>(value ?? null);
+    const [query, setQuery] = useState<string>(value ? toText(value) : '');
+    const [highlightedIndex, setHighlightedIndex] = useState(-1);
     const [isOpen, setIsOpen] = useState(false);
+
+    if (!options) {
+        return (
+            <Skeleton>
+                <InputField value={``} onValueChange={() => {
+                }}/>
+            </Skeleton>
+        );
+    }
+
+    const filteredOptions = options.filter(option => query === toText(value ?? option) ? true : toText(option).toLowerCase().includes(query.toLowerCase()));
+
+    const handleSelect = (option: T) => {
+        setSelected(option);
+        onValueChange(option);
+        setQuery(toText(option));
+        setIsOpen(false);
+    }
+
+    const handleKeyDown = (e: React.KeyboardEvent) => {
+        if (!options.length) return;
+
+        if (e.key === "ArrowDown") {
+            e.preventDefault();
+            e.stopPropagation();
+            setHighlightedIndex((prev) =>
+                prev < filteredOptions.length - 1 ? prev + 1 : 0
+            );
+        }
+
+        if (e.key === "ArrowUp") {
+            e.preventDefault();
+            e.stopPropagation();
+            setHighlightedIndex((prev) =>
+                prev > 0 ? prev - 1 : filteredOptions.length - 1
+            );
+        }
+
+        if (e.key === "Enter") {
+            e.preventDefault();
+            e.stopPropagation();
+            if (highlightedIndex >= 0) {
+                handleSelect(filteredOptions[highlightedIndex]);
+                setHighlightedIndex(-1);
+            } else {
+                setIsOpen(true);
+            }
+        }
+
+        if (e.key === "Escape") {
+            e.preventDefault();
+            e.stopPropagation();
+            setHighlightedIndex(-1);
+            setQuery(value ? toText(value) : '');
+            setIsOpen(false);
+        }
+    };
 
     return (
         <div
@@ -38,11 +102,12 @@ export default function SelectField({
                     {icon}
                 </label>
                 <input
-                    value={value}
-                    onChange={e => onValueChange(e.target.value)}
+                    value={query}
+                    onChange={e => setQuery(e.target.value)}
                     id={id}
                     className={`flex-1 outline-0 placeholder-input-text-placeholder`}
                     type="text"
+                    onKeyDown={handleKeyDown}
                     placeholder={placeholder}/>
             </div>
             <AutoHeight open={isOpen}>
@@ -50,20 +115,20 @@ export default function SelectField({
                     <PopoverDivider/>
                     <div className={`p-2 max-h-60 overflow-y-scroll`}>
                         {
-                            options.map(option =>
-                                <div
-                                    key={option}
-                                    onClick={() => {
-                                        onValueChange(option);
-                                        setIsOpen(false);
-                                    }}
-                                    className={`flex items-center rounded-md hover:bg-popover-element-hover py-2.5 px-2 cursor-pointer`}>
+                            filteredOptions.map((option, i) =>
+                                <div key={`select-option-${i}`}
+                                     tabIndex={0}
+                                     onClick={() => handleSelect(option)}
+                                     className={clsx(
+                                         `flex items-center rounded-md hover:bg-popover-element-hover py-2.5 px-2 cursor-pointer`,
+                                         highlightedIndex === i && `bg-popover-element-hover`
+                                     )}>
                                     <label className={`opacity-0 px-3 text-input-text-placeholder`}>
                                         {icon}
                                     </label>
                                     <div
                                         id={id}
-                                        className={`flex-1 outline-0 placeholder-input-text-placeholder -mx-4`}>{option}</div>
+                                        className={`flex-1 outline-0 placeholder-input-text-placeholder -mx-4`}>{toText(option)}</div>
                                 </div>
                             )
                         }
