8833fd72b8f4217b95ec0d299cc19814379837ab
Marco77577 <marco.studer@srf.ch>
2026-03-26 15:30:36 +0100

    Wire up the manage organizations link in the Profile component

--- frontend/app/components/header/profile/Profile.tsx
+++ frontend/app/components/header/profile/Profile.tsx
@@ -4,16 +4,18 @@ import {Building2, LogOut, Pencil} from "lucide-react";
 import PopoverListElement from "@/app/components/header/profile/PopoverListElement";
 import PopoverDivider from "@/app/components/header/profile/PopoverDivider";
 import ProfileTrigger from "@/app/components/header/profile/ProfileTrigger";
-import {Popover} from "@/app/components/header/profile/Popover";
+import {Popover, PopoverContext} from "@/app/components/header/profile/Popover";
 import {Box, Skeleton} from "@radix-ui/themes";
 import {useOrganizationsQuery} from "@/app/context/api/queries/organizations";
 import {useSetOrganizationId} from "@/app/context/OrganizationProvider";
+import {useRouter} from "next/navigation";
 
 interface ProfileProps {
     alwaysOpen?: boolean;
 }
 
 export default function Profile({alwaysOpen = false}: ProfileProps) {
+    const router = useRouter();
     const auth = useAuth();
     const {data: organizations, isLoading} = useOrganizationsQuery();
     const setOrganizationId = useSetOrganizationId();
@@ -29,47 +31,57 @@ export default function Profile({alwaysOpen = false}: ProfileProps) {
                 <ProfileTrigger/>
             </Popover.Trigger>
             <Popover.Content align={`right`}>
-                {isLoading && (
-                    <div>
-                        <Skeleton className={`mx-2 rounded-lg`}>
-                            <Box>
-                                <div className={`m-2 h-14`}/>
-                            </Box>
-                        </Skeleton>
-                        <PopoverDivider/>
-                    </div>
-                )}
-                {!isLoading && organizations && organizations.length > 0 && (
-                    <div className={`contents`}>
-                        {
-                            organizations.map(
-                                organization => {
-                                    return (
-                                        <PopoverListElement
-                                            key={organization.id}
-                                            title={organization.displayName}
-                                            subtitle="GmbH"
-                                            icon={Building2}
-                                            onClick={() => setOrganizationId(organization)}/>
-                                    )
-                                }
-                            )
-                        }
-                        <PopoverDivider/>
-                    </div>
-                )}
-                <PopoverListElement
-                    title="Manage Organizations"
-                    subtitle="Create and delete organizations."
-                    icon={Pencil}/>
-                <PopoverDivider/>
-                <PopoverListElement
-                    title="Logout"
-                    color="error"
-                    onClick={() => {
-                        auth.removeUser().then();
-                    }}
-                    icon={LogOut}/>
+                <PopoverContext.Consumer>
+                    {({close}) => (
+                        <>
+                            {isLoading && (
+                                <div>
+                                    <Skeleton className={`mx-2 rounded-lg`}>
+                                        <Box>
+                                            <div className={`m-2 h-14`}/>
+                                        </Box>
+                                    </Skeleton>
+                                    <PopoverDivider/>
+                                </div>
+                            )}
+                            {!isLoading && organizations && organizations.length > 0 && (
+                                <div className={`contents`}>
+                                    {
+                                        organizations.map(
+                                            organization => {
+                                                return (
+                                                    <PopoverListElement
+                                                        key={organization.id}
+                                                        title={organization.displayName}
+                                                        subtitle="GmbH"
+                                                        icon={Building2}
+                                                        onClick={() => setOrganizationId(organization)}/>
+                                                )
+                                            }
+                                        )
+                                    }
+                                    <PopoverDivider/>
+                                </div>
+                            )}
+                            <PopoverListElement
+                                title="Manage Organizations"
+                                subtitle="Create and delete organizations."
+                                icon={Pencil}
+                                onClick={() => {
+                                    router.push(`/organization`);
+                                    close();
+                                }}/>
+                            <PopoverDivider/>
+                            <PopoverListElement
+                                title="Logout"
+                                color="error"
+                                onClick={() => {
+                                    auth.removeUser().then();
+                                }}
+                                icon={LogOut}/>
+                        </>
+                    )}
+                </PopoverContext.Consumer>
             </Popover.Content>
         </Popover>
     )
