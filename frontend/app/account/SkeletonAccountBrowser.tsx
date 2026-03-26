bd7dc83944fd79f9232aa3ade304ff9ebb7a09ab
Marco77577 <marco.studer@srf.ch>
2026-03-26 12:00:27 +0100

    Remove hover effects from account skeletons

--- frontend/app/account/SkeletonAccountBrowser.tsx
+++ frontend/app/account/SkeletonAccountBrowser.tsx
@@ -9,7 +9,7 @@ export default function SkeletonAccountBrowser({className}: React.HTMLAttributes
             {
                 Array.from({length: 5}).map((_, i) => (
                     <div key={`skeleton-account-${i}`}
-                         className={`relative flex items-center gap-6 md:py-2.5 md:px-3 rounded-lg hover:bg-popover-element-hover cursor-pointer hover:**:data-options:opacity-100`}>
+                         className={`relative flex items-center gap-6 md:py-2.5 md:px-3 rounded-lg`}>
                         <Skeleton>
                             <div
                                 className={`w-full flex items-center gap-2 py-1`}>
--- frontend/app/account/SkeletonCategoryBrowser.tsx
+++ frontend/app/account/SkeletonCategoryBrowser.tsx
@@ -10,7 +10,7 @@ export default function SkeletonCategoryBrowser({className}: React.HTMLAttribute
             {
                 Array.from({length: 5}).map((_, i) => (
                     <div key={`skeleton-category-${i}`}
-                         className={`relative flex items-center gap-6 md:py-2.5 md:px-3 rounded-lg hover:bg-popover-element-hover cursor-pointer hover:**:data-options:opacity-100`}
+                         className={`relative flex items-center gap-6 md:py-2.5 md:px-3 rounded-lg`}
                          style={{paddingLeft: `${10 * (i === 0 ? 0 : 2)}px`}}>
                         <Skeleton>
                             <div className={`w-full flex items-center gap-2 py-1`}>
